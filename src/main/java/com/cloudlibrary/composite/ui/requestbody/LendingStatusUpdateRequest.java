9a32456c267ff9a79f1e5fc6a3271c4f9a5046d2
yongCloude <djkye0001@naver.com>
2022-04-14 17:24:25 +0900

    [create] lendingStatus 필드 관련 UpdateRequest 추가

---
+++ src/main/java/com/cloudlibrary/composite/ui/requestbody/LendingStatusUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.cloudlibrary.composite.ui.requestbody;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class LendingStatusUpdateRequest {
+    private String lendingStatus;
+}
