04f67fcff1e8cb2bf9bda0d760f5260bad27e401
yongCloude <djkye0001@naver.com>
2022-04-21 22:24:25 +0900

    [update] patch에 대한 mapper 함수 정의

--- src/main/java/com/cloudlibrary/composite/application/mapper/MapperForPatch.java
+++ src/main/java/com/cloudlibrary/composite/application/mapper/MapperForPatch.java
@@ -1,14 +1,22 @@
 package com.cloudlibrary.composite.application.mapper;
 
-import com.cloudlibrary.composite.application.service.CompositeOperationUseCase;
+import com.cloudlibrary.composite.application.domain.Composite;
 import com.cloudlibrary.composite.infrastructure.mysql.entity.CompositeEntity;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 
 @Mapper(componentModel = "spring")
 public interface MapperForPatch {
-    void updateBookStatusFromCompositeEntity(CompositeOperationUseCase.BookStatusUpdateCommand dto, @MappingTarget CompositeEntity entity);
-    void updateLendingStatusFromCompoisteEntity(CompositeOperationUseCase.LendingStatusUpdateCommand dto,@MappingTarget CompositeEntity entity);
-    void updateReservationInfoFromCompositeEntity(CompositeOperationUseCase.ReservationInfoUpdateCommand dto, @MappingTarget CompositeEntity entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateBookStatusFromCompositeEntity(Composite dto, @MappingTarget CompositeEntity entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateLendingStatusFromCompoisteEntity(Composite dto,@MappingTarget CompositeEntity entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateReservationInfoFromCompositeEntity(Composite dto, @MappingTarget CompositeEntity entity);
 }
