12875da64e5a3ffad62d6fb489da0816d92b9567
yongCloude <djkye0001@naver.com>
2022-04-21 10:25:46 +0900

    [update] service 구현체 생성

--- src/main/java/com/cloudlibrary/composite/application/service/CompositeService.java
+++ src/main/java/com/cloudlibrary/composite/application/service/CompositeService.java
@@ -1,4 +1,47 @@
 package com.cloudlibrary.composite.application.service;
 
-public class CompositeService {
+import com.cloudlibrary.composite.infrastructure.mysql.repository.CompositeEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CompositeService implements CompositeReadUseCase, CompositeOperationUseCase{
+
+    private CompositeEntityRepository compositeEntityRepository;
+
+    @Autowired
+    public CompositeService(CompositeEntityRepository compositeEntityRepository) {
+        this.compositeEntityRepository = compositeEntityRepository;
+    }
+
+    @Override
+    public FindCompositeResult createComposite(CompositeCreateCommand command) {
+        return null;
+    }
+
+    @Override
+    public FindCompositeResult getCompositeByBookId(BookFindQuery query) {
+        return null;
+    }
+
+    @Override
+    public List<FindCompositeResult> getCompositeListAll() {
+        return null;
+    }
+
+    @Override
+    public FindCompositeResult updateBookStatus(CompositeUpdateCommand command) {
+        return null;
+    }
+
+    @Override
+    public FindCompositeResult updateLendingStatus(CompositeUpdateCommand command) {
+        return null;
+    }
+
+    @Override
+    public FindCompositeResult updateReservationInfo(CompositeUpdateCommand command) {
+        return null;
+    }
+
 }
