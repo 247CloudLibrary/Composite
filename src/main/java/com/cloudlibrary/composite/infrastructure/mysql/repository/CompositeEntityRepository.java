55c335e8c439481b4ffd2a2951bf416dc5bcf3d8
yongCloude <djkye0001@naver.com>
2022-04-22 01:01:45 +0900

    [update] CrudRepository에서 JpaRepository로 변경

--- src/main/java/com/cloudlibrary/composite/infrastructure/mysql/repository/CompositeEntityRepository.java
+++ src/main/java/com/cloudlibrary/composite/infrastructure/mysql/repository/CompositeEntityRepository.java
@@ -1,9 +1,15 @@
 package com.cloudlibrary.composite.infrastructure.mysql.repository;
 
 import com.cloudlibrary.composite.infrastructure.mysql.entity.CompositeEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CompositeEntityRepository extends CrudRepository<CompositeEntity, Long> {
+import java.util.List;
 
 
+public interface CompositeEntityRepository extends JpaRepository<CompositeEntity, Long> {
+
+
+    List<CompositeEntity> findAllByPublisher(String publisher);
+    List<CompositeEntity> findAllByCategory(String category);
+    List<CompositeEntity> findAllByAuthor(String author);
 }
