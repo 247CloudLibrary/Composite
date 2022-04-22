818ba07dad38a6bc0d9a316c771d7a23540f7a22
yongCloude <djkye0001@naver.com>
2022-04-22 15:27:35 +0900

    [doc] EnableJpaRepositories 어노테이션 추가

--- src/main/java/com/cloudlibrary/composite/CompositeApplication.java
+++ src/main/java/com/cloudlibrary/composite/CompositeApplication.java
@@ -3,11 +3,13 @@ package com.cloudlibrary.composite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @EnableWebMvc
 @EnableFeignClients
+@EnableJpaRepositories
 @SpringBootApplication
 public class CompositeApplication {
 
